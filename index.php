bc306ec1515947e505217e1c5adda87022dfa438
fadeeva-marina <marinka24-97@mail.ru>
2017-10-12 21:38:34 +0400

    lab1

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Рецепты</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="css/magnific-popup.css">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="css/templatemo-style.css">                                   <!-- Templatemo style -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          
+          <![endif]-->
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+</head>
+    <body>        
+        <div class="container">
+            <div class="row">
+                <div class="tm-left-right-container">
+                    <!-- Left column: logo and menu -->
+                    <div class="tm-blue-bg tm-left-column">                        
+                        <div class="tm-logo-div text-xs-center">
+                           <!--  <img src="img/tm-neaty-logo.png" alt="Logo"> -->
+							<a href="#welcome"><img src="img/logo.png" alt="Logo"></a>
+                            <h1 class="tm-site-name"><a href="#welcome">Рецепт</a></h1>		
+                        </div>
+                        <nav class="tm-main-nav">
+                            <ul class="tm-main-nav-ul">
+                                <li class="tm-nav-item">
+                                    <a href="#welcome" class="tm-nav-item-link">Главная</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="#bakerygallery" class="tm-nav-item-link">Выпечка</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="#dessertgallery" class="tm-nav-item-link">Десерты</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="#drinkgallery" class="tm-nav-item-link">Напитки</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="#subscribe" class="tm-nav-item-link">Подписаться на нас</a>
+                                </li>
+                            </ul>
+                        </nav>                                         
+                    </div> <!-- Left column: logo and menu -->
+                    
+                    <!-- Right column: content -->
+                    <div class="tm-right-column">
+                        <figure>
+                            <img src="img/neaty-01.jpg" alt="Header image" class="img-fluid">    
+                        </figure>
+
+                        <div class="tm-content-div">
+                            <!-- Welcome section -->
+                            <section id="welcome" class="tm-section">
+                                <header>
+                                    <h2 class="tm-blue-text tm-welcome-title tm-margin-b-45">Добро пожаловать</h2>
+                                </header>
+                                <p>Приветствуем вас на нашем сайте. У нас вы сможете найти рецепты различных десертов, выпечки, а так же сладких (а может иногда и не сладких) напитков. Так же вы можете подписаться на рассылку, чтобы наши вкусные рецепты радовали вас каждый день!</p>
+								<div class="row"> 
+								<div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 push-lg-4 push-md-5">
+								<p>Мы обычные люди, которые любят готовить простые, но вкусные и красивые блюда. И нам очень хочется делиться этим с вами! Надеемся, вам понравится!</p>
+								</div>
+								<div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 pull-lg-8 pull-md-7 tm-about-img-container">
+                                        <img src="img/neaty-02.jpg" alt="Image" class="img-fluid">    
+                                </div> 
+								</div>
+                            </section>
+
+
+                            <!-- Gallery One section -->     
+                            <section id="bakerygallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Выпечка</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/03.jpg"><img src="img/03.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 1</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/04.jpg"><img src="img/04.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+										<span>Рецепт 2</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/05.jpg"><img src="img/05.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 3</span>										
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/06.jpg"><img src="img/06.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+										<span>Рецепт 4</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/07.jpg"><img src="img/07.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+										<span>Рецепт 5</span>
+                                    </div>   
+                                </div>
+                            </section>
+
+                            <!-- Second Gallery section -->     
+                            <section id="dessertgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Десерты</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/08.jpg"><img src="img/08.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+										<span>Рецепт 1</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/09.jpg"><img src="img/09.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+										<span>Рецепт 2</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/10.jpg"><img src="img/10.jpg" alt="Image" class="img-fluid tm-img-tn"></a>   
+									<span>Рецепт 3</span>										
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Third Gallery section -->     
+                            <section id="drinkgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Напитки</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1  textimage">
+                                        <a href="img/11.jpg"><img src="img/11.jpg" alt="Image" class="img-fluid tm-img-tn"></a>   
+										<span>Рецепт 1</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1  textimage">
+                                        <a href="img/12.jpg"><img src="img/12.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 2</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/13.jpg"><img src="img/13.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 3</span>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/14.jpg"><img src="img/14.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 4</span>
+                                    </div>       
+                                    <div class="tm-img-container tm-img-container-1 textimage">
+                                        <a href="img/15.jpg"><img src="img/15.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+										<span>Рецепт 5</span>
+                                    </div> 									
+                                </div>
+                            </section>
+
+                            <!-- Contact Us section -->
+                            <section id="subscribe" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Подписаться на рассылку</h2></header>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <form action="#contact" method="post" class="contact-form">
+                                            <div class="form-group">
+                                                <input type="" id="contact_name" name="contact_name" class="form-control" placeholder="Имя"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="" id="contact_email" name="contact_email" class="form-control" placeholder="Email"  required/>
+                                            </div>                                     
+                                            <button type="submit" class="float-right tm-button">Send</button>
+                                        </form>    
+                                    </div>
+                                    
+                                    <div class="col-lg-6 tm-contact-right">
+                                        <p>
+                                        Укажите свое имя и email и получайте ежедневно по одному из наших рецептов. 
+                                        </p>
+                                    </div>
+                                </div>
+                                
+                            </section>
+                            <footer>
+                                <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2017</span> Your Company 
+								| Designed by M</p>
+                            </footer>
+                        </div>  
+                        
+                    </div> <!-- Right column: content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+                
+        <!-- load JS files -->
+		<script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+        <script src="js/jquery.magnific-popup.min.js"></script>     <!-- Magnific pop-up (http://dimsemenov.com/plugins/magnific-popup/) -->
+        <script src="js/jquery.singlePageNav.min.js"></script>      <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+        <script>     
+       
+            $(document).ready(function(){
+
+                // Single page nav
+                $('.tm-main-nav').singlePageNav({
+                    'currentClass' : "active",
+                    offset : 20
+                });
+
+                // Magnific pop up
+                $('.tm-gallery-1').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-2').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-3').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-current-year').text(new Date().getFullYear());                
+            });
+        </script>               
+</body>
+</html>
